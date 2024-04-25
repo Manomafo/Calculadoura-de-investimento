e476943494fbbacdc63d592d5072a904cfd125e0
Manomafo <matthieustmart@gmail.com>
2024-04-25 14:58:20 -0300

    Començando a codar JavaScript

---
+++ src/investmentGoals.js
@@ -0,0 +1,12 @@
+function convertToMonthlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  staringAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "montlhy",
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = "monthly"
+) {}
